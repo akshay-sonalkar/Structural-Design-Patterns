3d70485c0ed9bae4c72d79151ce4f322757ca2ca
Akshay Sonalkar <akshay.sonalkar@contractor.everycure.org>
2025-02-20 14:46:02 +0530

    Proxy design pattern

--- Proxy/src/Main.java
+++ Proxy/src/Main.java
@@ -1,4 +1,13 @@
-
+// Main.java
 public class Main {
-    
+    public static void main(String[] args) {
+        Image image = new ProxyImage("test_image.jpg");
+
+        // Image will be loaded from disk
+        image.display();
+        System.out.println("");
+
+        // Image will not be loaded from disk
+        image.display();
+    }
 }
